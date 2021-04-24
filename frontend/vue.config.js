4e6a6582e7dd0dbcb8c04b2ffe6b8b9bc447f695
Cubicroot <github@cubicroot.xyz>
2021-04-24 18:42:02 +0200

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     devServer: {
         disableHostCheck: true
-    },
-    build: {
-        index: path.resolve(__dirname, '/app/index.html'),
-        assetsPublicPath: "/",
-        assetsSubDirectory: "src/assets"
-    },
-    publicPath: ''
+    }
 }
