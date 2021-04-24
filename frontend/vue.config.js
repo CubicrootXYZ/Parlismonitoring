178ee2ff9bd6db2caf65f08b388213e112c9369a
Cubicroot <github@cubicroot.xyz>
2021-04-24 14:46:29 +0200

    build settings

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
     devServer: {
         disableHostCheck: true
     },
+    build: {
+        index: path.resolve(__dirname, '/app/index.html')
+        assetssPublicPath: "/",
+        assetsSubDirectory: "src/assets"
+    }
     publicPath: ''
 }
